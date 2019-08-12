53cfc4a714d3f80e0aebc779599f44c1bfb2d101
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2019-08-12 19:00:12 +0200

    knn

--- theThird.py
+++ theThird.py
@@ -53,3 +53,49 @@ def read_magic_data(file_directory):
   data[:,:10] = x
   data = data.astype(np.dtype(float))
   return data, headers, b
+
+
+
+def print_stats(estimator, X_train, y_train, X_test, y_test):
+  y_pred_train = estimator.predict(X_train)
+  y_pred_test = estimator.predict(X_test)
+  print('Train Set Accuracy : ', accuracy_score(y_train, y_pred_train))
+  print('Train Set Precision : ', precision_score(y_train, y_pred_train))
+  print('Train Set Recall : ', recall_score(y_train, y_pred_train))
+  print('Train F-Score for each class : ', f1_score(y_train, y_pred_train, average=None))
+  print('Train Mean F-Score for both classes : ',f1_score(y_train, y_pred_train, average='macro'))
+  print('Train Confusion Matrix : ', confusion_matrix(y_train, y_pred_train))
+  print('----------------------------------------------------------------------')
+  print('Test Set Accuracy : ', accuracy_score(y_test, y_pred_test))
+  print('Test Set Precision : ', precision_score(y_test, y_pred_test))
+  print('Test Set Recall : ', recall_score(y_test, y_pred_test))
+  print('Test F-Score for each class : ', f1_score(y_test, y_pred_test, average=None))
+  print('Test Mean F-Score for both classes : ',f1_score(y_test, y_pred_test, average='macro'))
+  print('Test Confusion Matrix : ', confusion_matrix(y_test, y_pred_test))
+  print('----------------------------------------------------------------------')
+
+
+
+#KNN
+def kNearestNeighborsFunction(X_train, y_label_train, X_test, y_label_test, start_n, end_n):
+  parameters = {'n_neighbors': list(range(start_n, end_n))}
+  KNNC =  KNeighborsClassifier(n_jobs = num_jobs)
+  gKnn = GridSearchCV(KNNC, parameters,scoring = 'f1_macro', cv = 5, n_jobs = num_jobs)
+  gKnn.fit(X_train, y_label_train)
+  print('Best N found at ', gKnn.best_params_)
+  print('Best Mean F-Score ', gKnn.best_score_)
+  ns = gKnn.cv_results_['param_n_neighbors']
+  ns_score = gKnn.cv_results_['mean_test_score']
+  plt.title('Mean F-Score with different neighbors')
+  plt.plot(ns, ns_score)
+  plt.xlabel('# of neighbours')
+  plt.ylabel('Validation Mean F-Score')
+  plt.show()
+  print_stats(gKnn, X_train, y_label_train, X_test, y_label_test)
+
+
+
+
+
+
+
